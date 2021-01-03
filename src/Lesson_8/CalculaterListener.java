ba6f3c8e8ba19f0161f67a6be0128614e741bc3c
VadimHome <vadimtsh@gmail.com>
2021-01-03 16:16:55 +0300

    Lesson_8

--- src/Lesson_8/CalculaterListener.java
+++ src/Lesson_8/CalculaterListener.java
@@ -1,5 +1,8 @@
 package Lesson_8;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,34 +10,66 @@ import java.awt.event.ActionListener;
 public class CalculaterListener implements ActionListener {
     private final JTextField textField;
 
-    public CalculaterListener (JTextField textField) {
+    public CalculaterListener(JTextField textField) {
         this.textField = textField;
     }
 
     public void actionPerformed(ActionEvent actionEvent) {
-        String value=textField.getText();
+        String value = textField.getText();
+        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+        ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("Nashorn");
         String[] operators;
-        if (value.contains("+")){
-            operators=value.split("\\+");
+        if (value.contains("^")) {
+            operators = value.split("\\^");
             textField.setText(
                     String.valueOf(
-                            Integer.parseInt(operators[0] )+ Integer.parseInt(operators[1])
+                            Math.pow(Float.parseFloat(operators[0]), Float.parseFloat(operators[1]))
                     )
             );
-        }else if (value.contains("-")){
-            operators=value.split("-");
-            textField.setText(
-                    String.valueOf(
-                            Integer.parseInt(operators[0] )- Integer.parseInt(operators[1])
-                    )
-            );
-        }else if (value.contains("*")){
-            operators=value.split("\\*");
-            textField.setText(
-                    String.valueOf(
-                            Integer.parseInt(operators[0]) * Integer.parseInt(operators[1])
-                    )
-            );
+        }else {
+            Object expResult = null;
+            try {
+                expResult = scriptEngine.eval(value);
+            } catch (ScriptException e) {
+                e.printStackTrace();
+            }
+            textField.setText(String.valueOf(expResult));
         }
+//        if (value.contains("+")){
+//            operators=value.split("\\+");
+//            textField.setText(
+//                    String.valueOf(
+//                            Float.parseFloat(operators[0] )+ Float.parseFloat(operators[1])
+//                    )
+//            );
+//        }else if (value.contains("-")){
+//            operators=value.split("-");
+//            textField.setText(
+//                    String.valueOf(
+//                            Float.parseFloat(operators[0] )- Float.parseFloat(operators[1])
+//                    )
+//            );
+//        }else if (value.contains("*")){
+//            operators=value.split("\\*");
+//            textField.setText(
+//                    String.valueOf(
+//                            Float.parseFloat(operators[0]) * Float.parseFloat(operators[1])
+//                    )
+//            );
+//        } else if (value.contains("^")){
+//            operators=value.split("\\^");
+//            textField.setText(
+//                    String.valueOf(
+//                            Math.pow(Float.parseFloat(operators[0]),Float.parseFloat(operators[1]))
+//                    )
+//            );
+//        }else if (value.contains("/")){
+//            operators=value.split("/");
+//            textField.setText(
+//                    String.valueOf(
+//                            Float.parseFloat(operators[0])/Float.parseFloat(operators[1])
+//                    )
+//            );
+//        }
     }
 }
--- src/Lesson_8/Calculator.java
+++ src/Lesson_8/Calculator.java
@@ -33,19 +33,22 @@ public class Calculator extends JFrame {
 
     private JPanel initBottom() {
         JPanel bottom = new JPanel();
-        bottom.setLayout(new GridLayout(5, 3));
+        bottom.setLayout(new GridLayout(7, 3));
         ActionListener operatorsButtonListener = new OperatorsButtonListener(textField);
         for (int i = 1; i <= 9; i++) {
             JButton btn = new JButton(String.valueOf(i));
             btn.addActionListener(operatorsButtonListener);
             bottom.add(btn);
         }
-        JButton submit = new JButton("=");
-        submit.addActionListener(new CalculaterListener(textField));
-        bottom.add(submit);
         JButton zero = new JButton("0");
         zero.addActionListener(operatorsButtonListener);
         bottom.add(zero);
+        JButton point = new JButton(".");
+        point.addActionListener(operatorsButtonListener);
+        bottom.add(point);
+        JButton submit = new JButton("=");
+        submit.addActionListener(new CalculaterListener(textField));
+        bottom.add(submit);
         JButton clear = new JButton("c");
         clear.addActionListener(new ActionListener() {
             @Override
@@ -63,6 +66,34 @@ public class Calculator extends JFrame {
         JButton multiple = new JButton("*");
         multiple.addActionListener(operatorsButtonListener);
         bottom.add(multiple);
+        JButton division = new JButton("/");
+        division.addActionListener(operatorsButtonListener);
+        bottom.add(division);
+        JButton braketL = new JButton("(");
+        braketL.addActionListener(operatorsButtonListener);
+        bottom.add(braketL);
+        JButton braketR = new JButton(")");
+        braketR.addActionListener(operatorsButtonListener);
+        bottom.add(braketR);
+        JButton sqrt =new JButton("\u221A");
+        sqrt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                textField.setText(
+                        String.valueOf(
+                                Math.sqrt(
+                                        Float.parseFloat(
+                                                textField.getText()
+                                        )
+                                )
+                        )
+                );
+            }
+        });
+        bottom.add(sqrt);
+        JButton pow = new JButton("^");
+        pow.addActionListener(operatorsButtonListener);
+        bottom.add(pow);
         return bottom;
     }
 }
